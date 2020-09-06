4997aa0565dd8ca9d13a87517f7fc024161504c7
drincon2 <drincon2@miners.utep.edu>
2020-09-06 00:14:53 -0600

    Wrote inital reader and parser methods

--- shell/Shell.py
+++ shell/Shell.py
@@ -1,42 +1,54 @@
 #!/usr/bin/env python3
 
-#Libraries 
+# Libraries 
 import os
 import sys
 import re
 import gc
 
-#Shell process
+# Shell process
 def  shell():
    line = {}
    status = None 
    while status:
    	print("$ ")
-   	#Read
+   	# Read
    	line = sh_reader()
-   	#Parse
+   	# Parse
    	args = sh_parser(line)
-   	#Execute
+   	# Execute
    	status = sh_exec(args)
-   	#Free space 
+   	# Free space 
    	gc.collect()
    	
-#Make reader (this will read the commands written in the shell)
+# Reader (this will read the commands written in the shell)
 def sh_reader():
+   # Line dictionary
+   line = sys.argv
    
-#Make parser (this will execute the commands from the tokenizer)
+   return line    	
+   
+# Parser (this will tokenize the commands from the shell)
 def sh_parser(line): 
-
-#Make exec for built_in commands (cd, exit)
+   #Token dictionary 
+   tokens = {}
+   
+   for arg in line:
+      tokens[arg] = re.split(" ", arg)
+      
+   return token
+   
+# Execute built_in commands (cd, exit)
 def sh_exec(args):
+   # Use loop to determine whether execute a shell or built-in command 
 
-#Make pipes
+# Pipes
 def sh_pipes():
 
-#Make exec for commands
+# Execute commands
 def sh_exec_nativ(args):
    cpid = os.fork()
-   #Fork failed
+   # Fork failed
    if cpid < 0:
       os.write(2, ("Fork failed, returning %d\n" % cpid).encode())
       sys.exit(1)
@@ -45,11 +57,11 @@ def sh_exec_nativ(args):
    elif cpid > 0:
       os.write(1, ("I am parent. Pid: %d. Child Pid: %d\n" % (os.getpid(),cpid)).encode())
 
-#Main 
+# Main 
 def main(argc, argv):
-   #Initialize 
-   #Interpret 
-   #Terminate 
+   # Initialize 
+   # Interpret 
+   # Terminate 
    shell()
 
 
