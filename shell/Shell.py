dabc116b96e0c7d91f779730677d7464eecd5319
drincon2 <drincon2@miners.utep.edu>
2020-09-08 17:04:52 -0600

    Fixed error that ignored exit command, currently working on cd command

--- shell/Shell.py
+++ shell/Shell.py
@@ -7,27 +7,24 @@ import re
 
 # Shell process
 def shell():
-    status = 1
+   status = 1
     
-    while status == 1:
-       # Read user input for commands
-       line = input(os.getcwd() + "$ ")
-       # Parse user input
-       args = sh_parser(line)
-       # Execute commands
-       status = sh_exec(args)
+   while status == 1:
+      # Read user input for commands
+      line = input(os.getcwd() + "$ ")
+      # Parse user input
+      args = sh_parser(line)
+      # Execute commands
+      status = sh_exec(args)
        
     
 # Shell parser
 def sh_parser(line):
-  # Tokens 
-  tokens = {}
+   # Parse user input 
+   tokens = line.split(" ")
+    
+   return tokens
   
-  # Parse user input
-  for arg in line.split(" "):
-     tokens[arg] = arg
-     
-  return tokens
   
 # Execute commands
 def sh_exec(args):
@@ -36,7 +33,7 @@ def sh_exec(args):
       return 1
    
    # Check for built-in commands
-   for command in args:
+   for command in range(len(args)):
       # cd 
       if args[command] == "cd":
          os.execve(str(os.getcwd), args, os.environ)
@@ -44,6 +41,8 @@ def sh_exec(args):
       if args[command] == "exit":
          print("Closing shell ...")
          return 2
+         
+   print("command failed or invalid command...")
    
    return 1
 
