b72bbc43db8ba7072f3edd0efd9ab1f1801f605e
drincon2 <drincon2@miners.utep.edu>
2020-09-08 22:19:16 -0600

    Completed cd command and added catch error for invalid directory

--- shell/Shell.py
+++ shell/Shell.py
@@ -5,13 +5,14 @@ import os
 import sys
 import re
 
+
 # Shell process
 def shell():
    status = 1
     
    while status == 1:
       # Read user input for commands
-      line = input(os.getcwd() + "$ ")
+      line = input(os.getcwd() + "$ ")       
       # Parse user input
       args = sh_parser(line)
       # Execute commands
@@ -22,9 +23,8 @@ def shell():
 def sh_parser(line):
    # Parse user input 
    tokens = line.split(" ")
-    
+     
    return tokens
-  
   
 # Execute commands
 def sh_exec(args):
@@ -35,14 +35,21 @@ def sh_exec(args):
    # Check for built-in commands
    for command in range(len(args)):
       # cd 
-      if args[command] == "cd":
-         os.execve(str(os.getcwd), args, os.environ)
+      if args[command] == "cd" and args[command + 1] != None:
+         # Change to directory specified by user
+         try:
+            os.chdir(args[command + 1])
+            return 1 
+         # Prompt error message whenever directory is invalid
+         except FileNotFoundError:
+            print("bash: cd: "+ args[command + 1] + ": No such file or directory")
+            return 1
       # exit
       if args[command] == "exit":
          print("Closing shell ...")
          return 2
          
-   print("command failed or invalid command...")
+   print("invalid command...")
    
    return 1
 
